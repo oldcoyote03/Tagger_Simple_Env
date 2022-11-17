2b6c81db9f33278f830b285ae5e1cc86b099170e
oldcoyote03 <nickolas.vela@gmail.com>
2022-11-16 20:52:39 -0800

    db url and missing commit msg

--- utils/push-pull.sh
+++ utils/push-pull.sh
@@ -2,12 +2,20 @@
 
 # ./push-pull.sh -m "commit message"
 
-while getopts m flag
+while getopts m: flag
 do
     case "${flag}" in
         m) msg=${OPTARG};;
     esac
 done
+
+if [ -z "$msg" ]
+then
+    echo "missing commit message -m"
+    exit 1
+else
+    echo "commit message: $msg"
+fi
 
 echo "push"
 git add -A
@@ -16,7 +24,7 @@ git push --set-upstream origin develop
 
 sleep 10
 
-echo "pull"
-docker exec flask-api git pull
+#echo "pull"
+#docker exec flask-api git pull
 
 sleep 5
