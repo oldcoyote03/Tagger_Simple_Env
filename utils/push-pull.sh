6fd2cf24dd1a8873be2c4a9dbd321a195da73b19
oldcoyote03 <nickolas.vela@gmail.com>
2022-11-16 20:36:26 -0800

    remove build

--- utils/push-pull.sh
+++ utils/push-pull.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-while getopts m:t:c flag
+# ./push-pull.sh -m "commit message"
+
+while getopts m flag
 do
     case "${flag}" in
         m) msg=${OPTARG};;
-        t) tag=${OPTARG};;
-        c) cache=1;;
     esac
 done
 
@@ -13,16 +13,6 @@ echo "push"
 git add -A
 git commit -m "$msg"
 git push --set-upstream origin develop
-
-sleep 10
-
-echo "build"
-if [ -z "$cache" ]
-then
-    docker build --no-cache -t tagger-api:$tag .
-else
-    docker build -t tagger-api:$tag .
-fi
 
 sleep 10
 
